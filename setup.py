1a985d789c0ad83f4e9f5da2290b7b4f5bc03f72
Chris Barnes <barnesc@janelia.hhmi.org>
2019-07-11 20:43:50 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/clbarnes/yarqueue",
-    version="0.1.0",
+    version="version='0.1.1'",
     zip_safe=False,
 )
--- yarqueue/__init__.py
+++ yarqueue/__init__.py
@@ -17,7 +17,7 @@ from .serializer import Pickle, Json
 
 __author__ = """Chris L. Barnes"""
 __email__ = "barnesc@janelia.hhmi.org"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.1.1'"
 
 __all__ = [
     "Queue",
